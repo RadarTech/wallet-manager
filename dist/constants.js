dc6c57e6881b74e1c2d7711983ea62c000ffd30b
Cavan <cavan@radarrelay.com>
2018-05-24 20:17:22 -0600

    Update description and more linting

--- dist/constants.js
+++ dist/constants.js
@@ -2,18 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const types_1 = require("./types");
 exports.DEFAULT_DERIVATION_PATH = `m/44'/60'/0'/0`;
+exports.INFURA_API_KEY = ''; // Set API key (if applicable)
 exports.PUBLIC_RPC_PROVIDER_URLS = (connection) => {
     switch (connection) {
         case types_1.InfuraNetwork.Mainnet:
             return [
-                `https://mainnet.infura.io/radar`,
-                `https://pmainnet.infura.io/radar` // TODO: ${process.env.INFURA_API_KEY}
+                `https://mainnet.infura.io/${exports.INFURA_API_KEY}`,
+                `https://pmainnet.infura.io/${exports.INFURA_API_KEY}`
             ];
         case types_1.InfuraNetwork.Kovan:
         case types_1.InfuraNetwork.Rinkeby:
         case types_1.InfuraNetwork.Ropsten:
             return [
-                `https://${connection.toString()}.infura.io/radar` // TODO: ${process.env.INFURA_API_KEY}
+                `https://${connection.toString()}.infura.io/${exports.INFURA_API_KEY}`
             ];
         default:
             return [
